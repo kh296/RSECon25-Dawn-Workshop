d609e4830dd8ca837de2d8ec6eface5dff6288fe
K. Harrison <kh296@users.noreply.github.com>
2025-09-26 15:54:22 +0100

    Added comments

--- examples/run_mnist_classify_ddp.sh
+++ examples/run_mnist_classify_ddp.sh
@@ -5,53 +5,93 @@
 #SBATCH --nodes=2              # number of nodes
 #SBATCH --gres=gpu:4         # number of allocated gpus per node
 #SBATCH --time=01:00:00        # total run time limit (HH:MM:SS)
+#
+# Script for running pytorch example,
+# based around MNIST classification, with use of distributed data parallel.
+#
+# It's assumed that the environment for running pytorch applications
+# can be set up with:
+# source ../envs/ai-setup.sh
+#
+# This script can be run interactively:
+#     ./run_mnist_classify_ddp.sh
+# or can be submitted to a Slurm batch system, substituting
+# valid project account for <project_account>:.
+#     sbatch --acount=<project_account> run_mnist_classify_ddp.sh
+T1=${SECONDS}
+echo "Job start on $(hostname): $(date)"
 
-########################
-# 0. Set environment variables that would usually be set by Slurm
-########################
+# Exit at first failure.
+set -e
+
+# Ensure that Slurm environment variables are set,
+# also if outside of a Slurm environment.
+#
+# The variables specifying number of tasks per node (SLURM_NTASKS_PER_NODE),
+# number of tasks (SLURM_NTASKS), and number of CPU cores per task
+# (SLURM_CPUS_PER_TASK) are used in defining task parallelisation.
+# These numbers depend on the number of nodes allocated, on the
+# number of GPUs per node, and on how GPUs are configured.
+#
+# On Dawn, if a single node is allocated, then it may be allocated with
+# 1, 2, 3, or 4 GPUs (but not with 0 GPUs).  If more than one node is
+# allocated, all must be allocated with all (4) GPUs.  If GPUs are
+# used in "FLAT" mode, the two stacks of each GPU are treated as two
+# root devices.  If GPUs are used in "COMPOSITE" mode, the two stacks
+# of each GPU are treated as a single root device.  For more information
+# about modes for Intel GPUs, see:
+# https://www.intel.com/content/www/us/en/docs/oneapi/optimization-guide-gpu/2024-1/exposing-device-hierarchy.html
+# https://www.intel.com/content/www/us/en/developer/articles/technical/flattening-gpu-tile-hierarchy.html
+#
+# The variables specifying node(s) allocated (SLURM_JOB_NODELIST)
+# and job id (SLURM_JOB_ID) are used to define the master address and
+# port for task parallelisation.
+
+# Default to 1 node allocated if running outside of Slurm.
 if [[ -z "${SLURM_NNODES}" ]]; then
     SLURM_NNODES=1
 fi
 
+# Determine number of root devices per GPU on Dawn.
+if [[ "COMPOSITE" == ${ZE_FLAT_DEVICE_HIERARCHY} ]]; then
+    DEVICES_PER_GPU=1
+else
+    DEVICES_PER_GPU=2
+fi
+
+# Determine number of tasks per node, with one task per GPU root device,
+# or defaulting to 1 if there are no GPUs.
 if [[ -z "${SLURM_GPUS_ON_NODE}" ]]; then
     SLURM_NTASKS_PER_NODE=1
 else
-    SLURM_NTASKS_PER_NODE=$((2*${SLURM_GPUS_ON_NODE}))
+    SLURM_NTASKS_PER_NODE=$((${SLURM_GPUS_ON_NODE}*${DEVICES_PER_GPU}))
 fi
+
+# Determine total number of tasks.
 SLURM_NTASKS=$((${SLURM_NNODES}*${SLURM_NTASKS_PER_NODE}))
 
-# Unset Slurm variables for sbatch submission from Slurm environment.
+# Determine number of CPU cores per task.
+export SLURM_CPUS_PER_TASK=$((${SLURM_CPUS_ON_NODE}/${SLURM_NTASKS_PER_NODE}))
+
+# Ensure that value assigned to SLURM_JOB_NODELIST.
+if [[ -z "${SLURM_JOB_NODELIST}" ]]; then
+    SLURM_JOB_NODELIST=$(hostname)
+fi
+
+# Ensure that value assigned to SLURM_JOB_ID.
+if [[ -z "${SLURM_JOB_ID}" ]]; then
+    SLURM_JOB_ID=5100
+fi
+
+# Unset and set Slurm variables for compatibility with srun.
 unset SLURM_MEM_PER_CPU
 unset SLURM_MEM_PER_NODE
+SLURM_EXPORT_ENV=ALL
 
-########################
-# 1. Load Environment
-########################
+# Perform environment setup.
 source ../envs/ai-setup.sh
 
-echo "==== Step: Python / mpiexec version check ===="
-echo "Which python: $(which python)"
-echo "Python version: $(python --version)"
-echo "PyTorch version: $(python -c 'import torch; print(torch.__version__)')"
-echo "Which mpiexec: $(which mpiexec)"
-mpiexec --version
-
-########################
-# 2. Print job environment information
-########################
-export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-echo "Node list: $SLURM_JOB_NODELIST"
-echo "WORLD_SIZE=${WORLD_SIZE}"
-#echo "Print environment variables (some may be large):"
-#env | sort
-
-########################
-# 3. Set Intel MPI/OFI related environment variables
-########################
-
-# https://www.intel.com/content/www/us/en/developer/articles/technical/flattening-gpu-tile-hierarchy.html
-export ZE_FLAT_DEVICE_HIERARCHY="FLAT"
-export ZE_AFFINITY_MASK="0,1,2,3,4,5,6,7"
+# Set Intel MPI/OFI related environment variables.
 
 # export I_MPI_OFFLOAD=1
 # export I_MPI_OFFLOAD_SYMMETRIC=0
@@ -76,48 +116,25 @@ unset CCL_CONFIGURATION_PATH_modshare
 export CCL_WORKER_OFFLOAD=1
 
 # Use sockets instead of drmfd.
+# See: https://uxlfoundation.github.io/oneCCL/env-variables.html#ccl-ze-ipc-exchange
 #export CCL_ZE_IPC_EXCHANGE=sockets
 export CCL_ZE_IPC_EXCHANGE=pidfd
-
-########################
-# 4. Generate hostfile
-########################
-if [[ -z "${SLURM_JOB_NODELIST}" ]]; then
-    hostname > mpi_hostfile.txt
-else
-    scontrol show hostnames $SLURM_JOB_NODELIST > mpi_hostfile.txt
-fi
-echo "Generated hostfile:"
-cat mpi_hostfile.txt
-
-########################
-# 5. Set MASTER_ADDR/PORT
-########################
-
-if [[ -z "${SLURM_JOB_ID}" ]]; then
-    MASTER_PORT=31415
-else
-    MASTER_PORT=$(( (SLURM_JOB_ID % 10000) + 10000 ))
-fi
-MASTER_ADDR=$(head -n1 mpi_hostfile.txt)
-
-########################
-# 6. Launch
-########################
-
-echo "==== Step: mpiexec start ===="
 
 echo
 echo "Checking/downloading dataset"
 python -c "import torchvision as tv; tv.datasets.MNIST('data', download=True)"
 
+# Generate file of host names.
+scontrol show hostnames $SLURM_JOB_NODELIST > mpi_hostfile.txt
+
 read -r -d "" CMD << EOS
-mpiexec -n $WORLD_SIZE -ppn $SLURM_NTASKS_PER_NODE -f mpi_hostfile.txt\
+mpiexec -n ${SLURM_NTASKS} -ppn $SLURM_NTASKS_PER_NODE -f mpi_hostfile.txt\
  python mnist_classify_ddp.py\
- --epochs 1\
- --dist-url $MASTER_ADDR\
- --dist-port $MASTER_PORT\
  --ntasks-per-node $SLURM_NTASKS_PER_NODE
+ --dist-url $(head -n1 mpi_hostfile.txt)\
+ --dist-port $(( (SLURM_JOB_ID % 10000) + 50000 ))\
+ --cpus-per-task ${SLURM_CPUS_PER_TASK}\
+ --epochs 1
 EOS
 echo
 echo "${CMD}"
